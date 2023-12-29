2eb04e672dba5171abfda189782b022e1769cbc8
root <root@healing.tube>
2023-12-29 10:57:42 +0000

    FIxed

--- config/db.config.js
+++ config/db.config.js
@@ -3,14 +3,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  database: "healing-tube",
-  // host: '135.181.104.107',
-  // user: 'root',
-  // password: 'JYq1Nyr2l7Cf',
-  // database: 'freedom-buzz'
+   host: '65.108.84.111',
+   user: 'root',
+   password: '4rEuNVOY2C5d',
+   database: 'healing-tube'
 });
 
 db.connect(function (err) {
