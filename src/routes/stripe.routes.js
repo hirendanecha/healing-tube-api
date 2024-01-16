c6f4241b5a91628cc36e9d5ed96640c749423dbb
Swapnil Opash <sra.opash@gmail.com>
2024-01-16 17:25:02 +0530

    create webhook api

--- src/routes/stripe.routes.js
+++ src/routes/stripe.routes.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const stripeController = require("../controllers/stripe.controller");
 
 router.post("/create-payment-intent", stripeController.createPaymentIntent);
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  stripeController.webhook
+);
 
 module.exports = router;
