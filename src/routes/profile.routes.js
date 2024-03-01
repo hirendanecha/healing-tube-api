3b437ed4f4f8c444446d8c66d8fd2796236ac8d1
Swapnil Opash <sra.opash@gmail.com>
2024-03-01 17:45:11 +0530

    fixed

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const profileController = require("./../controllers/profile.controller");
 const authorize = require("../middleware/authorize");
 
+router.get(
+  "/getGroupBasicDetails/:uniqueLink",
+  profileController.getGroupBasicDetails
+);
 router.use(authorize.authorization);
 router.get("/groupsAndPosts", profileController.groupsAndPosts);
 router.get("/getGroups", profileController.getGroups);
-router.get("/getGroupBasicDetails/:uniqueLink", profileController.getGroupBasicDetails);
 router.get("/getGroupPostById/:id", profileController.getGroupPostById);
-router.get("/getGroupFileResourcesById/:id", profileController.getGroupFileResourcesById);
+router.get(
+  "/getGroupFileResourcesById/:id",
+  profileController.getGroupFileResourcesById
+);
 
 module.exports = router;
